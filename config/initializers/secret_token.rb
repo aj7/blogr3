a2c7e7a97a2c33bc07d585aab981f064b50ff09f
aj7 <ackbar@ackadij.com>
2010-11-04 15:00:21 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogr3::Application.config.secret_token = 'f97a32065eac6ac265546aa1f6fdd075bf3f084773b419a2530686b607b1b475f05b6a3eb96030489fdc948817fb20d6f933b5752cfd19f0ff9507c30eb26af9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Blogr3::Application.config.session_store :cookie_store, :key => '_blogr3_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Blogr3::Application.config.session_store :active_record_store
